f7a98cfcc95625fb179005bdbc10cb85de902619
elsuizo <mnoblia@disroot.org>
2020-06-07 21:23:27 -0300

    add criterion dev dependencie and one benchmark

--- README.md
+++ README.md
@@ -16,12 +16,48 @@ Methods:
 
  - `det()`: Determinant of the matrix
  - `inverse()`: Inverse of the matrix
- - `norm2()`: norm of the matrix
- - `transpose()`: transpose of the matrix
- - `trace()`: trace of the matrix
+ - `norm2`: norm of the matrix
+
+## Benchmarks
+
+Using the criterion crate:
+
+https://github.com/bheisler/criterion.rs
+
+this are the results for one operation(6x6 matrix inverse):
 
 
-TODOS:
+```text
+   inverse 6x6             time:   [295.87 us 301.60 us 308.75 us]
+Found 18 outliers among 100 measurements (18.00%)
+   3 (3.00%) low severe
+   5 (5.00%) low mild
+   2 (2.00%) high mild
+   8 (8.00%) high severe
+```
+
+you can look the bench here: [bench](benches/bench_inverse.rs)
+
+
+The same Matrix and test but in Julia language:
+
+```text
+BenchmarkTools.Trial:
+  memory estimate:  80.33 KiB
+  allocs estimate:  1298
+  --------------
+  minimum time:     2.039 ms (0.00% GC)
+  median time:      2.267 ms (0.00% GC)
+  mean time:        2.348 ms (0.81% GC)
+  maximum time:     18.015 ms (76.06% GC)
+  --------------
+  samples:          2118
+  evals/sample:     1
+
+```
+
+
+## TODOS:
 
  - [ ] Eigenvalues and Eigenvectors
  - [ ] `expm()`: Exponential matrix implementation
---
+++ benches/bench_inverse.rs
@@ -0,0 +1,63 @@
+//-------------------------------------------------------------------------
+// @file bench_inverse.rs
+//
+// @date 06/07/20 20:33:40
+// @author Martin Noblia
+// @email mnoblia@disroot.org
+//
+// @brief
+// Some benchs for speed reference
+// @detail
+//
+//  Licence:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+//--------------------------------------------------------------------------
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use static_math::traits::LinearAlgebra;
+use static_math::matrix6x6::M66;
+
+fn inverse_test() {
+
+    let m = M66::new([
+        [1.0, 1.0, 3.0, 4.0, 9.0, 3.0],
+        [10.0, 10.0, 1.0, 2.0, 2.0, 5.0],
+        [2.0, 9.0, 6.0, 10.0, 10.0, 9.0],
+        [10.0, 9.0, 9.0, 7.0, 3.0, 6.0],
+        [7.0, 6.0, 6.0, 2.0, 9.0, 5.0],
+        [3.0, 8.0, 1.0, 4.0, 1.0, 5.0],
+    ]);
+
+    if let Some(result) = m.inverse() {
+        println!("m: {}", result);
+    }
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("inverse 6x6", |b| b.iter(|| inverse_test()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
+
+
+//-------------------------------------------------------------------------
+//                        benches
+//-------------------------------------------------------------------------
+// inverse 6x6             time:   [295.87 us 301.60 us 308.75 us]
+// Found 18 outliers among 100 measurements (18.00%)
+//   3 (3.00%) low severe
+//   5 (5.00%) low mild
+//   2 (2.00%) high mild
+//   8 (8.00%) high severe
+//

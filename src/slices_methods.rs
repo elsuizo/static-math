e0985b1ec54dede4d886573ca3ad2e82f52dac9d
elsuizo <mnoblia@disroot.org>
2020-06-28 00:19:43 -0300

    add `find_max_min()` generic method to slices_methods

--- src/slices_methods.rs
+++ src/slices_methods.rs
@@ -24,6 +24,25 @@
 //--------------------------------------------------------------------------
 use num::{Num, Float};
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct MaxMin<T> {
+    max: T,
+    min: T,
+}
+
+/// generic function to fin min and max values in a slice
+pub fn find_max_min<T: std::cmp::PartialOrd + Copy>(slice: &[T]) -> MaxMin<T> {
+    let mut max = &slice[0];
+    let mut min = &slice[0];
+
+    for index in 1..slice.len() {
+        if slice[index] < *min { min = &slice[index];}
+        if slice[index] > *max { max = &slice[index];}
+    }
+
+    MaxMin{max: *max, min: *min}
+}
+
 /// calculate the euclidean norm of the slice
 pub fn norm2<T: Num + Copy + Float>(slice: &[T]) -> T {
     slice.iter().fold(T::zero(), |n, &i| (i * i) + n).sqrt()
@@ -45,4 +64,26 @@ pub fn normalize<T: Float>(slice: &mut [T]) {
 /// project x in the direction of y
 pub fn project_x_over_y<T: Float + std::iter::Sum>(x: &[T], y: &[T]) -> T {
     dot(x, y) / dot(y, y)
+}
+
+//-------------------------------------------------------------------------
+//                        tests
+//-------------------------------------------------------------------------
+#[cfg(test)]
+mod test_slides_methods {
+
+    use crate::vector3::V3;
+    use crate::slices_methods::*;
+
+    #[test]
+    fn find_max_min_test() {
+        let v = V3::new([1, 10, 37]);
+
+        let result = find_max_min(&*v);
+
+        let expected = MaxMin{max: 37, min: 1};
+
+        assert_eq!(result, expected);
+
+    }
 }

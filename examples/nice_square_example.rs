11ad137577b811d14579cd5c6a0a0a19760ac23d
elsuizo <mnoblia@disroot.org>
2020-09-17 13:25:48 -0300

    add a nice example found in internet

---
+++ examples/nice_square_example.rs
@@ -0,0 +1,45 @@
+//-------------------------------------------------------------------------
+// @file nice_square_example.rs
+//
+// @date 09/17/20 13:11:50
+// @author Martin Noblia
+// @email mnoblia@disroot.org
+//
+// @brief
+//
+// @detail
+//
+// Licence MIT:
+// Copyright <year> <Martin Noblia>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.  THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//-------------------------------------------------------------------------
+#[macro_use]
+extern crate static_math;
+
+use static_math::matrix2x2::M22;
+// i see this tweet and i want prove that
+// https://twitter.com/FlammableMaths/status/1306303462607654914
+fn main() {
+
+    let m = m22_new!(3, 4;
+                     6, 8);
+    let m_square = m * m;
+    println!("m: {:}", m);
+    println!("m_square: {:}", m_square);
+}
+
